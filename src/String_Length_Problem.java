fba664bc27445d0cae4723473277c5390fad3c16
Sushil Sridhar <sushilsridhar16@gmail.com>
2020-02-13 07:53:26 +0530

    String length problem

---
+++ src/String_Length_Problem.java
@@ -0,0 +1,25 @@
+public class String_Length_Problem {
+
+    public static void main(String [] args) {
+        String a = "\u0041"; // --> Code point is how one represent characters
+
+
+        String b = "\uD835\uDD38"; // -->  Mathematical double-struck capital A
+
+        System.out.println(a);
+        System.out.println(b);
+
+        System.out.println(a.length());
+        System.out.println(b.length()); // returns 2, should be 1
+
+        System.out.println(a.codePointAt(0));
+
+        System.out.println(b.codePointAt(1));
+        System.out.println(b.codePointCount(0, b.length())); // returns correct length
+
+
+        //System.out.println(1/10);
+
+    }
+
+}
